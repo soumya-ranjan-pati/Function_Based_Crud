05e597a1cc44e67b6509f0d0882acc34689f40f1
soumya <soumya6367@gmail.com>
2022-07-20 08:57:02 -0700

    edit settimgs.py

--- crudproject/crudproject/settings.py
+++ crudproject/crudproject/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%gz*^!wx3%g^ymam+0fz2*m=bv^^d_=3p_5m&1ssc1yp#l9*7z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
